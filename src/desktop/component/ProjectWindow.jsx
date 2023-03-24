8fce292060cdd0ecb35629c3dd6a100459b75fee
hosam89 <hosamothman1@gmail.com>
2023-03-24 11:48:23 +0100

    a new window component should be added

---
+++ src/desktop/component/ProjectWindow.jsx
@@ -0,0 +1,17 @@
+import { AiOutlineClose, AiOutlineMinus } from "react-icons/ai";
+import { BsSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import "animate.css";
+const Window = () => {
+  return (
+    <div className="icons projectIcon">
+      <Link to="/">
+        <AiOutlineClose />
+        <BsSquare />
+        <AiOutlineMinus />
+      </Link>
+    </div>
+  );
+};
+
+export default Window;
