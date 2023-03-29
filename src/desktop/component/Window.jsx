bd13931c46e00094272b96a4a37ec25520ebd3c5
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-03-29 22:27:52 +0200

    try to do another window for projects

--- src/desktop/component/Window.jsx
+++ src/desktop/component/Window.jsx
@@ -2,6 +2,25 @@ import { AiOutlineClose, AiOutlineMinus } from "react-icons/ai";
 import { BsSquare } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import "animate.css";
+// to be do later from the Hiwan
+// import React from "react";
+
+// function MyComponent() {
+//   const styles = {
+//     ":root": {
+//       "--main-color": "#4285F4",
+//     },
+//     h1: {
+//       color: "var(--main-color)",
+//     },
+//   };
+
+//   return (
+//     <div style={styles}>
+//       <h1>This is a heading</h1>
+//       {/* rest of your component */}
+//     </div>
+//   );
 const Window = () => {
   return (
     <div className="icons">
